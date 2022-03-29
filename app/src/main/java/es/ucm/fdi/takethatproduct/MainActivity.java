8560d6b5e9713940cdb2ecfd39206a1b0fa42a32
JaimeMadFer <61852686+JaimeMadFer@users.noreply.github.com>
2022-03-29 11:56:03 +0200

    boton orden alfabético hecho (más o menos)

--- app/src/main/java/es/ucm/fdi/takethatproduct/MainActivity.java
+++ app/src/main/java/es/ucm/fdi/takethatproduct/MainActivity.java
@@ -3,6 +3,8 @@ package es.ucm.fdi.takethatproduct;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    boolean boton_pulsado = true;
+    ImageButton boton_orden_notas;
+
+    public void pulsado(View view)
+    {
+        boton_orden_notas = (ImageButton)findViewById(R.id.mainViewNotesOrderButton);
+
+        if (boton_pulsado)
+        {
+            boton_pulsado = false;
+            //Drawable d = getResources().getDrawable(R.drawable.z_a);
+            //boton_orden_notas.setBackground(d);
+            boton_orden_notas.setImageResource(android.R.drawable.arrow_up_float);
+            // R.drawable.z_a
+
+            // ordenar las notas en orden inverso
+            // ...
+        }
+        else
+        {
+            boton_pulsado = true;
+            boton_orden_notas.setImageResource(android.R.drawable.ic_menu_sort_alphabetically);
+            // R.drawable.a_z
+
+            // ordenar las notas en el orden normal
+            // ...
+        }
+    }
+
 }
