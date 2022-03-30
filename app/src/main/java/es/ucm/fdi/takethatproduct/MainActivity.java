be67b3c79ee11cb4dec9961025c29ff23f3a7f1d
JaimeMadFer <61852686+JaimeMadFer@users.noreply.github.com>
2022-03-30 13:20:49 +0200

    empezando a relacionar spinner (opciones de orden de las notas) con botón (orden normal o inverso)

--- app/src/main/java/es/ucm/fdi/takethatproduct/MainActivity.java
+++ app/src/main/java/es/ucm/fdi/takethatproduct/MainActivity.java
@@ -1,55 +1,38 @@
 package es.ucm.fdi.takethatproduct;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.loader.app.LoaderManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private NotePreviewListAdapter notePreviewListAdapter;
     RecyclerView notePreviewListView;
+
+    String[] orderNotesOptions = {"Alfabéticamente", "Por fecha de creación", "Recientes"};
+    Spinner spin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-
-    boolean boton_pulsado = true;
-    ImageButton boton_orden_notas;
-
-    public void pulsado(View view)
-    {
-        boton_orden_notas = (ImageButton)findViewById(R.id.mainViewNotesOrderButton);
-
-        if (boton_pulsado)
-        {
-            boton_pulsado = false;
-            //Drawable d = getResources().getDrawable(R.drawable.z_a);
-            //boton_orden_notas.setBackground(d);
-            boton_orden_notas.setImageResource(android.R.drawable.arrow_up_float);
-            // R.drawable.z_a
-
-            // ordenar las notas en orden inverso
-            // ...
-        }
-        else
-        {
-            boton_pulsado = true;
-            boton_orden_notas.setImageResource(android.R.drawable.ic_menu_sort_alphabetically);
-            // R.drawable.a_z
-
-            // ordenar las notas en el orden normal
-            // ...
-        }
         notePreviewListView = findViewById(R.id.notePreviewList);
         notePreviewListAdapter = new NotePreviewListAdapter(this);
 
@@ -65,6 +48,79 @@ public class MainActivity extends AppCompatActivity {
         dummyList.add("test");
         notePreviewListAdapter.setmNoteList((ArrayList<String>) dummyList);
         notePreviewListAdapter.notifyDataSetChanged();
+
+
+
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        //Spinner
+        spin = (Spinner) findViewById(R.id.mainViewOrderNotesOptionsSpinner);
+        spin.setOnItemSelectedListener(this);
+
+        //Creating the ArrayAdapter instance having the bank name list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item, orderNotesOptions);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+
+
+        boton_orden_notas = (ImageButton) findViewById(R.id.mainViewNotesOrderButton);
+
+    }
+
+    boolean boton_pulsado = true;
+    ImageButton boton_orden_notas;
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    public void pulsado(View view) {
+        // boton_orden_notas = (ImageButton) findViewById(R.id.mainViewNotesOrderButton);
+
+        if (boton_pulsado) {
+            boton_pulsado = false;
+
+            int g = boton_orden_notas.getLayoutDirection();
+
+            boton_orden_notas.setImageResource(android.R.drawable.arrow_up_float);
+            // R.drawable.z_a
+
+            // ordenar las notas en orden inverso
+            // ...
+        } else {
+            boton_pulsado = true;
+            boton_orden_notas.setImageResource(android.R.drawable.ic_menu_sort_alphabetically);
+            //boton_orden_notas.setPadding(0, 0, 0, 0);
+            //boton_orden_notas.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            //boton_orden_notas.setAdjustViewBounds(true);
+
+            //R.drawable.a_z
+
+
+            // ordenar las notas en el orden normal
+            // ...
+        }
+    }
+
+    //spin.setOnItemSelectedListener(new OnItemSelectedListener() {
+    //});
+
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
+        Toast.makeText(getApplicationContext(), orderNotesOptions[position], Toast.LENGTH_LONG).show();
+
+
+        int Hold = spin.getSelectedItemPosition() + 1 ;
+        Log.d("PRUEBA", "MENSAJE ITEM POS: " + Hold);
+
+        if (spin.getSelectedItem() == "Recientes")
+        {
+            //Log.d("PRUEBA", "MENSAJE NOMBRE ITEM: " + spin.getSelectedItem());
+            boton_orden_notas.setImageResource(android.R.drawable.arrow_down_float);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
 
     }
 
