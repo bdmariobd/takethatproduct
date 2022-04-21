3fb51272a3f5575fa324d08df8f00d092608ec45
Mario Blanco Dominguez <mario10_bd@hotmail.com>
2022-04-21 13:42:25 +0200

    add image to note

--- app/src/main/java/es/ucm/fdi/takethatproduct/NoteTotalViewActivity.java
+++ app/src/main/java/es/ucm/fdi/takethatproduct/NoteTotalViewActivity.java
@@ -6,16 +6,28 @@ import androidx.fragment.app.FragmentContainer;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ImageSpan;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import es.ucm.fdi.takethatproduct.integration.note.Note;
 
 public class NoteTotalViewActivity extends AppCompatActivity {
 
+    EditText noteText;
+    Bitmap bitmap = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +35,10 @@ public class NoteTotalViewActivity extends AppCompatActivity {
 
         Note note = (Note) getIntent().getSerializableExtra("note");
         EditText titleInput = findViewById(R.id.noteTotalViewTitle);
-        EditText bodyInput = findViewById(R.id.noteTotalViewBody);
+        noteText = findViewById(R.id.noteTotalViewBody);
         View searchProductsContainer = findViewById(R.id.searchProductsFragmentContainer);
         titleInput.setText(note.getTitulo(), TextView.BufferType.EDITABLE);
-        bodyInput.setText(note.getCuerpo(), TextView.BufferType.EDITABLE);
+        noteText.setText(note.getCuerpo(), TextView.BufferType.EDITABLE);
 
         for (Fragment fragment : getSupportFragmentManager().getFragments()) {
             getSupportFragmentManager().beginTransaction().remove(fragment).commit();
@@ -39,7 +51,7 @@ public class NoteTotalViewActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 note.setTitulo(titleInput.getText().toString());
-                note.setCuerpo(bodyInput.getText().toString());
+                note.setCuerpo(noteText.getText().toString());
                 Intent i = new Intent();
                 i.putExtra("noteResult", note);
                 setResult(0, i);
@@ -60,5 +72,39 @@ public class NoteTotalViewActivity extends AppCompatActivity {
 
             }
         });
+
+        findViewById(R.id.noteTotalViewAddElementsButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), 10);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        //Detects request codes
+        if(requestCode==10 && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = data.getData();
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                SpannableStringBuilder builder = new SpannableStringBuilder();
+                builder.append(noteText.getText().toString()==null?" ":noteText.getText().toString());
+                builder.setSpan(new ImageSpan(this, bitmap), builder.length() - 1, builder.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                noteText.setText(builder);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
